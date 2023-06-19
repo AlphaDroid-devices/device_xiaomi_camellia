5f3ce0af9812dc6640f5018c1640249a55b0c596
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-19 07:18:02 +0000

    camellia: Disable ART debug and optimize dexpreopt
     * don't include debug configs in art
     * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)
     * use non-debug dex2oat
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -259,6 +259,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
