c6465765ca0f6c49b223679517040c192c14f093
Naoko Shoto <mdigreget@gmail.com>
2023-11-17 22:12:22 +0700

    camellia: overlays: Aperture: Configure additional camera settings
    Co-authored-by:  Noah113-Allen <kingfighter2958@gmail.com>
    Co-authored-by:  NaokoShoto <mdigreget@gmail.com>
    Signed-off-by: luk1337  <priv.luk@gmail.com>

--- device.mk
+++ device.mk
@@ -322,6 +322,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureOverlayCamellia \
     CarrierConfigOverlayCamellia \
     FrameworksResOverlayCamellia \
     SettingsOverlayCamellia \
